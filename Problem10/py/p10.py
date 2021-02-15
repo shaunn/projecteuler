d5f1d34ac4d7ffee176ce17bd6d799aa61f9d036
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-02-15 13:57:54 -0500

    Problem 10 Completed using Python

--- Problem10/py/p10.py
+++ Problem10/py/p10.py
@@ -3,34 +3,29 @@
 # Find the sum of all the primes below two million.
 
 # The test
-# number = 10
+number = 10
 # Answer is 17
 
 # The real deal
-number = 2000000
+# number = 2000000
 
 # List of discovered primes
 primes = []
 
-# List of discovered prime factors
-prime_factors = []
-
 
 def main():
+    print("Searching for primes lower than", number)
     nth_prime = prime_factor_generator(number)
     print("The last prime below", number, "is", nth_prime[-1])
-    print(primes)
-    print(sum(primes))
+    print("Sum of prime:", sum(primes))
 
 
 def prime_factor_generator(_max):
     # Initial load of primes array
     if not primes:
-        # 1 is NOT a prime. 0 is not. 2 is the first. TIL
+        # 1 is NOT a prime. 0 is not either. 2 is the first. TIL
         _prime_candidate = 2
         primes.append(_prime_candidate)
-        # print("New prime:",_prime_candidate)
-    # "prime" the primes array! Get it?
     _prime_candidate = primes[-1]
     if _prime_candidate in primes:
         _prime_candidate = primes[-1] + 1
@@ -45,7 +40,6 @@ def prime_factor_generator(_max):
                 break
             else:
                 primes.append(_prime_candidate)
-                # print("New prime:",_prime_candidate)
     return primes
 
 
