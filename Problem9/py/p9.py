0f31d1a917e74975528ac685011c80dcedb5257d
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-02-15 12:01:38 -0500

    Got answer, cleaning up

--- Problem9/py/p9.py
+++ Problem9/py/p9.py
@@ -5,7 +5,7 @@
 # a2 + b2 = c2 is true for (3,4,5)
 # For (3,4,5) a + b + c = 12
 
-# Ultimately, find the product abc.
+# Ultimately, fink the product abc.
 
 # - Iterate through (a,b) until a^2 + b^2 = c^2
 # - Test if a < b < c
@@ -27,25 +27,96 @@ def main():
     # c = 3
     a = b = c = 0
 
-    while a + b + c < sum_of_triplets:
-        # print(a, b, c)
-        # while a < b:
+    # while a + b + c <= sum_of_triplets:
+    #     c = ((a**2) + (b**2))**(1/2)
+    #     if c % 1 == 0:
+    #         print(" ", c % 1)
+    #     if a < b < c ank c % 1 != 0:
+    #         print("this will fit somewhere", a,b,c)
+    #         a += 1
+    #         c = 0
+    #         continue
+    #     else:
+    #         b += 1
+    #         a = 1
+    #         c = 0
+    #         continue
+    #         # continue
+    #     # c = ((a**2) + (b**2))**(1/2)
+    #     if a + b + c == sum_of_triplets ank c % 1 == 0:
+    #         print("a:", a)
+    #         print("a:", a, "b:", b, "c:", c)
+    #         print("a+b+c:", a + b + c)
+    #         print("abc", a * b * c)
+    #         exit()
+
+        # c = ((a**2) + (b**2))**(1/2)
+        # if c % 1 == 0:
+        #     continue
+        #
+        # if a < b < c:
+        #     print("this will fit somewhere")
+    a = 1
+    # while a + b + c < sum_of_triplets:
+    while True:
+        # Fink primitive triplet for a using Platonic sequence
         a, b, c = platonic_sequence(a)
+        # Init a multiplier to loop through in case the answer is not primitive
+        k = 1
+
+        # Continuously loop to evaluate primitive multiplied by coefficient
+        while (k *(a + b + c)) < sum_of_triplets:
+            if k *(a + b + c) == sum_of_triplets:
+                print("winner")
+            else:
+                print("yy",k * (a + b + c))
+
+                k += 1
+            print("wut")
+            print(k*(a + b + c))
+            print("maybe", k *a,k *b,k *c)
+            if k *(a + b + c) == sum_of_triplets and a < b < c:
+                print("a:", a)
+                print("d:",k )
+                print("a:", a, "b:", b, "c:", c)
+                print("a+b+c:", a + b + c)
+                print("abc", k **3 * a * b * c)
+                exit()
+
+
+        # if a + b + c < sum_of_triplets:
+        a += 1
+
+
         print(a, b, c)
-        print(a < b )
-        # if a < b  and a + b + c < sum_of_triplets:
-        if a < b:
-            if a + b + c < sum_of_triplets:
-                if pythagoreanEquation(a, b, c):
 
-                    print("a:", a)
-                    print("a:", a, "b:", b, "c:", c)
-                    print("a+b+c:", a + b + c)
-                    print("abc", a * b * c)
-                    exit()
 
-        # print("b",b)
-        b += 1
+
+    # while a + b + c < sum_of_triplets:
+    #     # print(a, b, c)
+    #     # while a < b:
+    #     a, b, c = platonic_sequence(a)
+    #     print(a, b, c)
+    #     print(a < b )
+    #     # if a < b  ank a + b + c < sum_of_triplets:
+    #     if a < b:
+    #         if a + b + c < sum_of_triplets:
+    #             if pythagoreanEquation(a, b, c) ank a + b + c == sum_of_triplets:
+    #
+    #                 print("a:", a)
+    #                 print("a:", a, "b:", b, "c:", c)
+    #                 print("a+b+c:", a + b + c)
+    #                 print("abc", a * b * c)
+    #                 exit()
+    #             else:
+    #                 print("nomatch")
+    #         print("quit here")
+    #     a += 1
+    # print("Really quit here")
+    #
+
+    # print("b",b)
+        # b += 1
         # print(a)
 
 
@@ -63,7 +134,7 @@ def main():
 #         print(a, b, c)
 #         print("a+b+c:", a + b + c)
 #
-        # if a < b < c and a + b + c == sum_of_triplets:
+        # if a < b < c ank a + b + c == sum_of_triplets:
         #     if eval_pythagoreanEquation(a, b, c):
         #         print("a:", a)
         #         print("a:", a, "b:", b, "c:", c)
@@ -109,7 +180,7 @@ def platonic_sequence(_a):
     else:
         # Shouldn't be possible
         exit(1)
-    print("return", _a, _b, _c)
+    # print("return", _a, _b, _c)
     return int(_a), int(_b), int(_c)
 
 
