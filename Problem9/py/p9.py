dda2a96e80da71f96ddd908ff9f8326698234031
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-02-15 12:26:57 -0500

    Problem 9 Solved for Python

--- Problem9/py/p9.py
+++ Problem9/py/p9.py
@@ -16,157 +16,36 @@
 
 # Tst
 # sum_of_triplets = 12
-
 sum_of_triplets = 1000
 
 
 def main():
-    # Starting points for a < b < c
-    # a = 1
-    # b = 2
-    # c = 3
-    a = b = c = 0
-
-    # while a + b + c <= sum_of_triplets:
-    #     c = ((a**2) + (b**2))**(1/2)
-    #     if c % 1 == 0:
-    #         print(" ", c % 1)
-    #     if a < b < c ank c % 1 != 0:
-    #         print("this will fit somewhere", a,b,c)
-    #         a += 1
-    #         c = 0
-    #         continue
-    #     else:
-    #         b += 1
-    #         a = 1
-    #         c = 0
-    #         continue
-    #         # continue
-    #     # c = ((a**2) + (b**2))**(1/2)
-    #     if a + b + c == sum_of_triplets ank c % 1 == 0:
-    #         print("a:", a)
-    #         print("a:", a, "b:", b, "c:", c)
-    #         print("a+b+c:", a + b + c)
-    #         print("abc", a * b * c)
-    #         exit()
-
-        # c = ((a**2) + (b**2))**(1/2)
-        # if c % 1 == 0:
-        #     continue
-        #
-        # if a < b < c:
-        #     print("this will fit somewhere")
     a = 1
     # while a + b + c < sum_of_triplets:
     while True:
-        # Fink primitive triplet for a using Platonic sequence
+        # Find primitive triplet for 'a' using Platonic sequence
         a, b, c = platonic_sequence(a)
+
         # Init a multiplier to loop through in case the answer is not primitive
-        k = 1
+        k = 0
 
         # Continuously loop to evaluate primitive multiplied by coefficient
-        while (k *(a + b + c)) < sum_of_triplets:
-            if k *(a + b + c) == sum_of_triplets:
-                print("winner")
-            else:
-                print("yy",k * (a + b + c))
-
-                k += 1
-            print("wut")
-            print(k*(a + b + c))
-            print("maybe", k *a,k *b,k *c)
-            if k *(a + b + c) == sum_of_triplets and a < b < c:
-                print("a:", a)
-                print("d:",k )
-                print("a:", a, "b:", b, "c:", c)
-                print("a+b+c:", a + b + c)
-                print("abc", k **3 * a * b * c)
+        while (k * (a + b + c)) < sum_of_triplets:
+            k += 1
+            if k * (a + b + c) == sum_of_triplets and a < b < c:
+                # A solution has been found; inform the world
+                print("Primitive Triplet: ", a, b, c)
+                a = k * a
+                b = k * b
+                c = k * c
+                print("Primitive Multiplier:", k)
+                print("Solution Triplet: ", a,b,c)
+                print("a + b + c:", a + b + c)
+                print("a * b * c:", a * b * c)
                 exit()
 
-
-        # if a + b + c < sum_of_triplets:
+        # Increment 'a' before being processed at the start of the loop
         a += 1
-
-
-        print(a, b, c)
-
-
-
-    # while a + b + c < sum_of_triplets:
-    #     # print(a, b, c)
-    #     # while a < b:
-    #     a, b, c = platonic_sequence(a)
-    #     print(a, b, c)
-    #     print(a < b )
-    #     # if a < b  ank a + b + c < sum_of_triplets:
-    #     if a < b:
-    #         if a + b + c < sum_of_triplets:
-    #             if pythagoreanEquation(a, b, c) ank a + b + c == sum_of_triplets:
-    #
-    #                 print("a:", a)
-    #                 print("a:", a, "b:", b, "c:", c)
-    #                 print("a+b+c:", a + b + c)
-    #                 print("abc", a * b * c)
-    #                 exit()
-    #             else:
-    #                 print("nomatch")
-    #         print("quit here")
-    #     a += 1
-    # print("Really quit here")
-    #
-
-    # print("b",b)
-        # b += 1
-        # print(a)
-
-
-        # a = 1
-        #     print(a, b, c)
-        #
-        #     print("grok",a + b + c )
-        #     print("h:",a + b + c == sum_of_triplets)
-
-
-# def main():
-#     a = b = c = 0
-#     while a + b + c < sum_of_triplets:
-#         a, b, c = platonic_sequence(a)
-#         print(a, b, c)
-#         print("a+b+c:", a + b + c)
-#
-        # if a < b < c ank a + b + c == sum_of_triplets:
-        #     if eval_pythagoreanEquation(a, b, c):
-        #         print("a:", a)
-        #         print("a:", a, "b:", b, "c:", c)
-        #         print("a+b+c:", a + b + c)
-        #         print("abc", a * b * c)
-        #         exit()
-        #
-        # a += 1
-
-# print("wew lad, that didn't work. Going Euclid")
-#
-# m = 2
-# n = 1
-# a = b = c = 0
-#
-# while a + b + c < sum_of_triplets:
-#     a, b, c = euclids_variant(m,n)
-#     print(m,n)
-#     print(a,b,c)
-#     while m > n > 0:
-#         n +=1
-#     m+=1
-
-
-def euclids_variant(_m, _n):
-    # a=mn,  b=(m^2 - n^2)/2, c=(m^2 + n^2)/2
-    # m > n > 0
-    _a = int(_m * _n)
-    _b = int((_m ** 2 - _n ** 2) / 2)
-    _c = int((_m ** 2 + _n ** 2) / 2)
-    return _a, _b, _c
-
 
 def platonic_sequence(_a):
     _b = _c = 0
@@ -177,31 +56,7 @@ def platonic_sequence(_a):
     elif _a % 2 == 0:
         _b = (_a / 2) ** 2 - 1
         _c = (_a / 2) ** 2 + 1
-    else:
-        # Shouldn't be possible
-        exit(1)
-    # print("return", _a, _b, _c)
     return int(_a), int(_b), int(_c)
-
-
-def get_hypotenuse(_a, _b, _c=False):
-    # For validation purposes only
-    if _c:
-        print("here")
-        if _a ** 2 + _b ** 2 == _c ** 2:
-            return True
-    _c = (_a ** 2 + _b ** 2) ** (1 / 2)
-    print(_a, _b, _c)
-
-    return _c
-
-
-def pythagoreanEquation(_a, _b, _c = None):
-    if _c is None:
-        _c = (_a ** 2 + _b ** 2)**(1/2)
-    if _a ** 2 + _b ** 2 == _c ** 2:
-        return _a, _b, _c
-    return False
 
 
 if __name__ == "__main__":
