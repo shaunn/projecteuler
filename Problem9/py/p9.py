a4507e338a482b2c0b5bd974f77509ce3ea754c0
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-02-14 22:52:44 -0500

    Cleaned, but never really worked

--- Problem9/py/p9.py
+++ Problem9/py/p9.py
@@ -27,11 +27,8 @@ def main():
     c = 3
 
     while a + b + c < sum_of_triplets:
-        print(a, b, c)
         while a < b:
             c = get_hypotenuse(a, b)
-            print(c)
-            print(c%1)
             if (c).is_integer():
                 c = int(c)
                 if a + b + c > 1000:
@@ -43,87 +40,15 @@ def main():
             else:
                 a += 1
             b += 1
-        # a = 1
-            print("grok",a + b + c )
-            print("h:",a + b + c < sum_of_triplets)
-
-
-# def main():
-#     a = b = c = 0
-#     while a + b + c < sum_of_triplets:
-#         a, b, c = platonic_sequence(a)
-#         print(a, b, c)
-#         print("a+b+c:", a + b + c)
-#
-#         if a < b < c and a + b + c == sum_of_triplets:
-#             if eval_pythagoreanEquation(a, b, c):
-#                 print("a:", a)
-#                 print("a:", a, "b:", b, "c:", c)
-#                 print("a+b+c:", a + b + c)
-#                 print("abc", a * b * c)
-#                 exit()
-#
-#         a += 1
-
-# print("wew lad, that didn't work. Going Euclid")
-#
-# m = 2
-# n = 1
-# a = b = c = 0
-#
-# while a + b + c < sum_of_triplets:
-#     a, b, c = euclids_variant(m,n)
-#     print(m,n)
-#     print(a,b,c)
-#     while m > n > 0:
-#         n +=1
-#     m+=1
-
-
-def euclids_variant(_m, _n):
-    # a=mn,  b=(m^2 - n^2)/2, c=(m^2 + n^2)/2
-    # m > n > 0
-    _a = int(_m * _n)
-    _b = int((_m ** 2 - _n ** 2) / 2)
-    _c = int((_m ** 2 + _n ** 2) / 2)
-    return _a, _b, _c
-
-
-def platonic_sequence(_a):
-    _b = _c = 0
-    # Check if even or odd, then use the appropriate formulae
-    if _a % 2 == 1:
-        _b = (_a ** 2 - 1) / 2
-        _c = (_a ** 2 + 1) / 2
-    elif _a % 2 == 0:
-        _b = (_a / 2) ** 2 - 1
-        _c = (_a / 2) ** 2 + 1
-    else:
-        # Shouldn't be possible
-        exit(1)
-
-    return _a, _b, _c
 
 
 def get_hypotenuse(_a, _b, _c=False):
     # For validation purposes only
     if _c:
-        print("here")
         if _a ** 2 + _b ** 2 == _c ** 2:
             return True
     _c = (_a ** 2 + _b ** 2) ** (1 / 2)
-    print(_a, _b, _c)
-
     return _c
-
-
-# def pythagoreanEquation(_a, _b, _c = None):
-#     if _c is None:
-#         _c = (_a ** 2 + _b ** 2)**(1/2)
-#     print(_a,_b,_c)
-#     if _a ** 2 + _b ** 2 == _c ** 2:
-#         return _a, _b, _c
-#     return False
 
 
 if __name__ == "__main__":
