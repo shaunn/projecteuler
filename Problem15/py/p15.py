c1c00d5af58679ac8e818a3a869660cef3386586
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-02-19 19:19:42 -0500

    Problem 15 solved with python

--- Problem15/py/p15.py
+++ Problem15/py/p15.py
@@ -1,5 +1,6 @@
-# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
-#
+# Starting in the top left corner of a 2×2 grid, and only being able
+#   to move to the right and down, there are exactly 6 routes to the
+#   bottom right corner.
 #
 # How many such routes are there through a 20×20 grid?
 #
@@ -7,16 +8,38 @@
 #
 # Strategy
 #
-# - Starting from an origin point (x,y) represented by (0,0)
-# - Identify the endpoint (x+end,y+end) represented in the example as (1,1)
-# - (1,1) represents the end of a 2x2 grid assuming (0,0) is the origin
-# - Iterate through a combination of choices to either go right or down through each iteration
-# - ? Start with the most expensive path from (0,0) to (x+end,y+end)? and work backwards?
+# - Looked this up; using combination formula
+# - n!/(k!(n-k)!)
+# - where n is the number of squares to traverse
+# - k is the number or directions that can be taken per square
+
+
+# Test
+# x_max = 2
+# y_max = 2
+
+# # Real deal
+x_max = 20
+y_max = 20
 
 
 def main():
-    print("init")
+    # number_steps_to_finish
+    n = x_max + y_max
+    # number_of_moves_in_one_direction
+    k = 20  # right or down
 
+    combination_of_different_paths = int(factorial(n) / (factorial(k) * factorial(n - k)))
+    print(combination_of_different_paths)
+
+
+def factorial(_x):
+    _factorial = 1
+    while _x >= 0:
+        for i in range(1, _x + 1):
+            _factorial *= i
+        return _factorial
+    # Don't care to handle negatives
 
 
 if __name__ == "__main__":
