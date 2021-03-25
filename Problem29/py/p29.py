bd2f1f5199bfb6e5c2e3d101f494819156f83ea5
shaunnrosenberg <shaunnrosenberg@gmail.com>
2021-03-24 23:44:18 -0400

    Solved p29 with python

--- Problem29/py/p29.py
+++ Problem29/py/p29.py
@@ -1,27 +1,37 @@
 
 
 # Test
-number = 5
+# number = 5
 
 
 # Real Deal
-# number = 100
+number = 100
 
 result_list = []
 
 def main():
-    for a in range(1, number + 1):
-        for b in range(1, number + 1):
+    for a in range(2, number + 1):
+        for b in range(2, number + 1):
             result_list.append(a**b)
 
     # answer = list(set(result_list))
-    answer = list(set(result_list))
-    print("list", result_list)
-    print("answer list:", answer)
-    print("answer", len(answer))
+    answer = repeat_remover(result_list)
+    print("answer: ", sorted(answer))
+    print("answer: ", len(answer))
 
 def repeat_remover(_list):
+    _return_list=[]
+    print("list",_list)
+    _set = list(set(_list))
+    print("set",_set)
+    return sorted(_set)
+    # for n in _set:
+    #     print("n",n)
+    #     # print(_list.count(n))
+    #     if _list.count(n) == 1:
+    #         _return_list.append(n)
 
+    return _return_list
 
 if __name__ == "__main__":
     main()
